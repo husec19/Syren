d22c9bce37887c87e22c7d5456b8b65655b48d04
Deniz <pooh@hoops-macbook.local>
2013-10-02 18:51:35 -0700

    new features

--- Syren/Syren.cpp
+++ Syren/Syren.cpp
@@ -1,61 +1,68 @@
 // Syren.cpp : Defines the entry point for the console application.
-// Syren Media Player -Audio Streaming Engine- (ASE) Command Line Test Tool
+// Syren Media Player -Audio Streaming Engine- (ASE) Command Line Demo Tool
 // Deniz H.A, Sept, 2013
 
 #include "stdafx.h"
 #include <windows.h>
 #include <stdio.h>
 
-typedef int (*df_playAudioFile)(LPWSTR); 
+typedef int (*df_playAudioFile)(LPWSTR pathToFile); 
+typedef int (*df_playAudioList)(LPWSTR pathToDirectory); 
 
 int _tmain(int argc, _TCHAR* argv[])
 {
    df_playAudioFile playAudioFile;
+   df_playAudioList playAudioList;
    HINSTANCE syrenase;
    LPWSTR *szArgList;
    int nArgs;
    MSG msg;
- 
+   bool playFlag = false;  
+
+   ZeroMemory(&msg, sizeof(msg));
+
    szArgList = CommandLineToArgvW(GetCommandLineW(), &nArgs);
-   if( NULL == szArgList )
+   if(NULL == szArgList)
    {
       wprintf(L"CommandLineToArgvW failed\n");
       return 0;
    }
 
-   if(nArgs < 2) {
+   if((nArgs > 1) && wcscmp(szArgList[1], L"-plist") == 0) playFlag = true;
+	
+   if(nArgs < 2 || (nArgs==2 && playFlag)) {
 
-	   printf("\nSyren Media Player ASE Test Tool v1.0b - (c) Alex G, Sept 2013");
-	   printf("\nUsage: Syren(.exe) audiofile.[wma, mp3] \n");
-
+	   printf("\nSyren Media Player (ASE) Tool v1.01b, - October 2013");
+	   printf("\nUsage: Syren(.exe) -plist <directory name> or single audiofile.[-.M4A, -.MP3, -.WMA]\n");	   
 	   return (0);
    }
-
+  
    // ** Load SyrenASE.DLL into process 
    syrenase = LoadLibrary((LPCWSTR)L"SyrenASE.dll");
 
-   if (syrenase != 0);
- //  printf("library loaded!\n");
-   else
-   printf("*** SyrenASE.DLL, library failed to load ***!\n");
+   if(!syrenase)
+       printf("*** SyrenASE.DLL, library failed to load ***!\n");
 
-   // ** Accessing the real function via function pointer..  
-   playAudioFile = (df_playAudioFile) GetProcAddress(syrenase,"syrenPlayAudioFile");
+   // ** Accessing to real functions via function pointer..
+   // ** Now we can play the audio file or playlist with functions residing in SyrenASE.DLL
+   if(!playFlag) {
+       playAudioFile = (df_playAudioFile) GetProcAddress(syrenase,"syrenPlayAudioFile");
+       playAudioFile(szArgList[1]);
+   }
+   else {
+       playAudioList = (df_playAudioList) GetProcAddress(syrenase,"syrenPlayAudioList");
+       playAudioList(szArgList[2]);
+   }
 
-   // ** Now we can play the audio file with syrenPlayAudioFile function which resides in SyrenASE.DLL
-   playAudioFile((LPWSTR)szArgList[1]);
-
-   ZeroMemory(&msg, sizeof(msg));
-
-   // process Run Loop 
-   while (GetMessage(&msg, NULL, 0, 0))
-   {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+   // thread run Loop 
+   while (GetMessage(&msg, NULL, 0, 0)) {
+           
+	   TranslateMessage(&msg);
+       DispatchMessage(&msg);
    }
 
    LocalFree(szArgList);
 
-	return 0;
+	return (0);
 }
 
---
+++ Syren/Syrenmain.cpp
@@ -0,0 +1,9 @@
+
+// Syren Media Player - Audio Streaming Engine - Test Access Code
+
+
+int __cdecl main()
+{
+   return(1);
+}
+
